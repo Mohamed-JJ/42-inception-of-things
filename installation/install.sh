25ef4566d62ea6b7671e399a711682181a8f47aa
mohamed-JJ <simojarboue24@gmail.com>
2024-05-18 16:35:36 +0100

    initial commit and files structure

---
+++ installation/install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# get the package from the official k3s page
+curl -sfL https://get.k3s.io | sh -
---
+++ makefile
@@ -0,0 +1,5 @@
+install:
+	chmod 744 ./installation/install.sh
+	bash ./installation/install.sh
+
+start:
